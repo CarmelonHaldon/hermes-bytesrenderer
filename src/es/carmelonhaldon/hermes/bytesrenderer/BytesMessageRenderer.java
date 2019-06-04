de25291a269edcdaa4b8aebf7f5ee2c97b8081bb
CarmelonHaldon <carmelonhaldon@gmail.com>
2019-06-04 11:06:23 +0200

    Refactor...

--- src/es/carmelonhaldon/hermes/bytesrenderer/BytesMessageRenderer.java
+++ src/es/carmelonhaldon/hermes/bytesrenderer/BytesMessageRenderer.java
@@ -1,4 +1,4 @@
-package es.carmelonhaldon.hermes.mapbytesrenderer;
+package es.carmelonhaldon.hermes.bytesrenderer;
 
 import javax.jms.JMSException;
 import javax.jms.MapMessage;
@@ -7,11 +7,10 @@ import javax.swing.JComponent;
 import hermes.browser.dialog.message.MapBytesMessagePayloadPanel;
 import hermes.renderers.DefaultMessageRenderer;
 
-public class MapBytesMessageRenderer extends DefaultMessageRenderer {
+public class BytesMessageRenderer extends DefaultMessageRenderer {
 
 	protected JComponent handleMapMessage(MapMessage mapMessage) throws JMSException {
 
 		return new MapBytesMessagePayloadPanel(mapMessage, false);
 	}
-
 }
